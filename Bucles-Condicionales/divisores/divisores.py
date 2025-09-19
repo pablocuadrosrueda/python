b8bfcb73ba736eafaa407520be0336214fc93190
Pablo Cuadros Rueda <pcr00036@red.ujaen.es>
2025-09-19 13:07:01 +0200

    Arreglo de error en ejercicio

--- Bucles-Condicionales/divisores/divisores.py
+++ Bucles-Condicionales/divisores/divisores.py
@@ -5,7 +5,7 @@ while num <= 0:
 
 #Mostramos todos sus divisores, que teniendo en cuenta que el máximo divisor posible de un numero es su mitad o menos de la mitad para impares (sin contar el mismo) ... 
 print(f"Mostrando divisores del número {num}:")   
-for i in range(2,num//2):
+for i in range(1,num+1):
     if num%i == 0: 
         print(i, ",",end="")
 
